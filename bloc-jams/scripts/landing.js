09b3bf1b7e9d9ab6e19530a37fe02851dd20f6de
Ryan Ford <rford.nm@gmail.com>
2017-04-06 19:04:59 -0600

    assignment-8 fixed animatePOints function

--- bloc-jams/scripts/landing.js
+++ bloc-jams/scripts/landing.js
@@ -2,20 +2,18 @@
  
                  var points = document.getElementsByClassName('point');
  
-                 var revealPoint = function(points) {
-                     points.style.opacity = 1;
-                     points.style.transform = "scaleX(1) translateY(0)";
-                     points.style.msTransform = "scaleX(1) translateY(0)";
-                     points.style.WebkitTransform = "scaleX(1) translateY(0)";
+                 var revealPoint = function(index) {
+                     points[index].style.opacity = 1;
+                     points[index].style.transform = "scaleX(1) translateY(0)";
+                     points[index].style.msTransform = "scaleX(1) translateY(0)";
+                     points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
                  };
  
-       
-                 
- 
+    
                     for (var i = 0; i <= points.length; i++){
                         
                         
-                        revealPoint(points[i]);
+                        revealPoint(i);
                         
                         
                     }
